240614b85cb222218348b555579074c128af597c
mrzinofc <145413073+mrzinofc@users.noreply.github.com>
2024-07-22 22:29:10 -0300

    menu dinamico

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+
+let navBar = document.querySelector('#header')
+
+document.addEventListener('scroll', () => {
+    let scrollTop = window.scrollY
+    
+    if(scrollTop > 0){
+        navBar.classList.add('rolar')
+    } else {
+        navBar.classList.remove('rolar')    
+    }
+})
